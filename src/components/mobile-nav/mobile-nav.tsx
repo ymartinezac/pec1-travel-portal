d453db789c10376a782242a02a70493822505858
Yesenia <ymartinezac@uoc.edu>
2021-11-08 22:19:35 -0500

    ;kn

--- src/components/mobile-nav/mobile-nav.tsx
+++ src/components/mobile-nav/mobile-nav.tsx
@@ -14,10 +14,7 @@ const MobileNav = () => {
         
         <div className="mobile-nav">
             <h1>🇵🇷 ViajaPR </h1>
-            <Link to="/" onClick={handleClickHome}>Inicio</Link> 
-                <Link to="/buscar-tours" onClick={handleClickBuscarTours}>Buscar Tours</Link>
-                <Link to="/presentacion"  onClick={handleClickPresentacion}>Presentación</Link> 
-                <Link to="/enlaces" onClick={handleClickEnlaces}>Enlaces</Link>     
+              
         </div>
     )
 }
