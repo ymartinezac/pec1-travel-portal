e036ce582215c5cfd487330059bb8c6a6515fa05
yesenia <ymartinezac@uoc.edu>
2022-01-08 17:42:27 -0400

    lazy load top tours

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -7,49 +7,11 @@ import { NavLink } from "react-router-dom";
 
 const Snorkeling = React.lazy(() => import('../snorkeling/snorkeling'));
 const InfoGeneral = React.lazy(() => import('../info-general/info-general'));
-
-
-const puntuacionMedia = (tour: Tour) => {
-    var sum = 0;
-    tour.ratings.map(rating => { 
-        if(typeof rating.puntuacion == "number"){
-           sum += rating.puntuacion; 
-        }
-    })
-    const length = tour.ratings.length;
-    return (sum / length);
-}
-
-function compare( a, b ) {
-   
-    if ( puntuacionMedia(a) > puntuacionMedia(b) ){
-        console.log(puntuacionMedia(a) < puntuacionMedia(b));
-         return -1;
-    }
-    else if ( puntuacionMedia(a) < puntuacionMedia(b) ){
-        return 1;
-    }
-    else {
-        return 0;
-    }
-  }
-
-const compararPuntuaciones = ( tours: Array<Tour> ) => {
-    return tours.sort( compare );
-}
+const TopTours = React.lazy(() => import('../top-tours-grid/top-tours-grid'));
 
 const Home: React.FC<{tours: Tour[]}> = ({ tours }) => {
     window.scrollTo(0, 0);
-    
-    const renderTopTours = () => {
-        const sortedTours = compararPuntuaciones(tours);
-        const top3 = sortedTours.slice(0, 3);
-        return top3.map((tour: Tour) => {
-            return (
-                <TourCard tour={tour} />
-            );
-        })
-    }
+   
     return (
         <main className="home-main">
             <section className="main-landing">
@@ -58,16 +20,12 @@ const Home: React.FC<{tours: Tour[]}> = ({ tours }) => {
                     <h2>Isla del Encanto</h2>
                 </div>
             </section>
-            <section className="top-tour-grid">
-                <div className="top-tours-header">
-                    <h2>Tours más vendidos</h2>
-                    <NavLink to="/buscar-tours" className="ver-mas">Ver más</NavLink>
-                </div>
-             
-                    {renderTopTours()} 
+          
+            <React.Suspense fallback={<p>Loading...</p>}>
+                <TopTours tours={tours} />
                
-                
-            </section> 
+            </React.Suspense>
+      
             
             <React.Suspense fallback={<p>Loading...</p>}>
                 <Snorkeling />
---
+++ src/components/top-tours-grid/top-tours-grid.tsx
@@ -0,0 +1,70 @@
+
+import React from "react";
+import Tour from "../../models/tour";
+import TourCard from "../tour-card/tour-card";
+import { NavLink } from "react-router-dom";
+
+const puntuacionMedia = (tour: Tour) => {
+    var sum = 0;
+    tour.ratings.map(rating => { 
+        if(typeof rating.puntuacion == "number"){
+           sum += rating.puntuacion; 
+        }
+    })
+    const length = tour.ratings.length;
+    return (sum / length);
+}
+
+function compare( a, b ) {
+   
+    if ( puntuacionMedia(a) > puntuacionMedia(b) ){
+        console.log(puntuacionMedia(a) < puntuacionMedia(b));
+         return -1;
+    }
+    else if ( puntuacionMedia(a) < puntuacionMedia(b) ){
+        return 1;
+    }
+    else {
+        return 0;
+    }
+  }
+
+const compararPuntuaciones = ( tours: Array<Tour> ) => {
+    return tours.sort( compare );
+}
+
+const TopTours: React.FC<{tours: Tour[]}> = ({ tours }) => {
+    window.scrollTo(0, 0);
+    
+    const renderTopTours = () => {
+        const sortedTours = compararPuntuaciones(tours);
+        const top3 = sortedTours.slice(0, 3);
+        return top3.map((tour: Tour) => {
+            return (
+                <TourCard tour={tour} />
+            );
+        })
+    }
+    return (
+      
+            <section className="top-tour-grid">
+                <div className="top-tours-header">
+                    <h2>Tours más vendidos</h2>
+                    <NavLink to="/buscar-tours" className="ver-mas">Ver más</NavLink>
+                </div>
+             
+                    {renderTopTours()} 
+               
+                
+            </section> 
+            
+       
+            
+    
+
+    )
+  
+    
+}
+
+export default TopTours;
